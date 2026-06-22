1732a6203acd58d7c214029bd7695331a9a65834
karans18 <karannikumbh18@gmail.com>
2026-06-23 00:57:22 +0530

    Added product APIs

--- controller/movieController.js
+++ controller/movieController.js
@@ -12,39 +12,52 @@ export const getMovie = async (req, res) => {
     req.query;
 
   let filter = {};
-  if (year) {
-    filter.year = Number(year);
-  } else if (minYear && maxYear) {
-    filter.year = {
-      $gte: Number(minYear),
-      $lte: Number(maxYear),
-    };
-  } else if (minYear) {
-    filter.year = {
-      $gte: Number(minYear),
-    };
-  } else if (maxYear) {
-    filter.year = {
-      $lte: Number(maxYear),
-    };
+  //   if (year) {
+  //     filter.year = Number(year);
+  //   } else if (minYear && maxYear) {
+  //     filter.year = {
+  //       $gte: Number(minYear),
+  //       $lte: Number(maxYear),
+  //     };
+  //   } else if (minYear) {
+  //     filter.year = {
+  //       $gte: Number(minYear),
+  //     };
+  //   } else if (maxYear) {
+  //     filter.year = {
+  //       $lte: Number(maxYear),
+  //     };
+  //   }
+  if (minYear || maxYear) {
+    filter.year = {};
+
+    if (minYear) filter.year.gte = Number(minYear);
+
+    if (maxYear) ilter.year.gte = Number(minYear);
   }
 
   //rating
-  if (rating) {
-    filter.ratings = Number(rating);
-  } else if (minRating && maxRating) {
-    filter.ratings = {
-      $gte: Number(minRating),
-      $lte: Number(maxRating),
-    };
-  } else if (minRating) {
-    filter.ratings = {
-      $gte: Number(minRating),
-    };
-  } else if (maxRating) {
-    filter.ratings = {
-      $lte: Number(maxRating),
-    };
+  //   if (rating) {
+  //     filter.ratings = Number(rating);
+  //   } else if (minRating && maxRating) {
+  //     filter.ratings = {
+  //       $gte: Number(minRating),
+  //       $lte: Number(maxRating),
+  //     };
+  //   } else if (minRating) {
+  //     filter.ratings = {
+  //       $gte: Number(minRating),
+  //     };
+  //   } else if (maxRating) {
+  //     filter.ratings = {
+  //       $lte: Number(maxRating),
+  //     };
+  //   }
+
+  if (minRating && maxRating) {
+    rating.filter = {};
+    if (minRating) filter.rating.$gte = Number(minRating);
+    if (maxRating) filter.rating.$lte = Number(minRating);
   }
 
   const order = sort === "desc" ? -1 : 1;
